feae236ed410c09e1d93c5342d846444e4b725d2
xiedi <xiedi8895@dingtalk.com>
2020-08-10 20:33:15 +0800

    new version

---
+++ AMap3DMap/Additions/AMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Interop;
+
+namespace Com.Amap.Api.Maps
+{
+    public sealed partial class AMap : global::Java.Lang.Object
+    {
+
+        public partial interface IOnCameraChangeListener : IJavaObject, IJavaPeerable
+        {
+
+        }
+
+    }
+
+}
