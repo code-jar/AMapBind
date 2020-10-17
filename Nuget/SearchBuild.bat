53b896aed77d0b4083fedfb9c223ea4c6c2b4bd8
code-jar <ricarod_jar@outlook.com>
2020-10-17 18:34:44 +0800

    new version

--- Nuget/SearchBuild.bat
+++ Nuget/SearchBuild.bat
@@ -1,4 +1,4 @@
 msbuild ../AMapSearch/AMapSearch.csproj /t:Clean /t:Rebuild /v:minimal /p:Configuration=Release;DebugType=none;TargetFrameworkVersion=v4.4;OutDir=bin\Release\lib\monoandroid44
 
-nuget pack amap.xamarin.android.search.nuspec -Version 7.4.0
+nuget pack amap.xamarin.android.search.nuspec -Version 7.6.0
 pause
