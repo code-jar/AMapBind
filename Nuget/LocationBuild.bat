41430872e23a08c92dd2a3297f93475a12b63f0e
code-jar <ricarod_jar@outlook.com>
2022-01-08 11:34:55 +0800

    new version

--- Nuget/LocationBuild.bat
+++ Nuget/LocationBuild.bat
@@ -1,4 +1,4 @@
 msbuild ../AMapLocation/AMapLocation.csproj /t:Clean /t:Rebuild /v:minimal /p:Configuration=Release;DebugType=none;TargetFrameworkVersion=v11.0;OutDir=bin\Release\lib\monoandroid11.0
 
-nuget pack amap.xamarin.android.location.nuspec -Version 5.4.0
+nuget pack amap.xamarin.android.location.nuspec -Version 5.6.2
 pause
--- Nuget/SearchBuild.bat
+++ Nuget/SearchBuild.bat
@@ -1,4 +1,4 @@
 msbuild ../AMapSearch/AMapSearch.csproj /t:Clean /t:Rebuild /v:minimal /p:Configuration=Release;DebugType=none;TargetFrameworkVersion=v11.0;OutDir=bin\Release\lib\monoandroid11.0
 
-nuget pack amap.xamarin.android.search.nuspec -Version 7.9.0
+nuget pack amap.xamarin.android.search.nuspec -Version 8.1.0
 pause
