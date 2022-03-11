f2c3cfe99e169e26be3abda36026d60d8501f084
code-jar <ricarod_jar@outlook.com>
2022-03-11 15:48:04 +0800

    new version

--- Nuget/3DMapBuild.bat
+++ Nuget/3DMapBuild.bat
@@ -1,4 +1,4 @@
 msbuild ../AMap3DMap/AMap3DMap.csproj /t:Clean /t:Rebuild /v:minimal /p:Configuration=Release;DebugType=none;TargetFrameworkVersion=v11.0;OutDir=bin\Release\lib\monoandroid11.0
 
-nuget pack amap.xamarin.android.3dmap.nuspec -Version 8.1.0
+nuget pack amap.xamarin.android.3dmap.nuspec -Version 9.1.0
 pause
--- Nuget/LocationBuild.bat
+++ Nuget/LocationBuild.bat
@@ -1,4 +1,4 @@
 msbuild ../AMapLocation/AMapLocation.csproj /t:Clean /t:Rebuild /v:minimal /p:Configuration=Release;DebugType=none;TargetFrameworkVersion=v11.0;OutDir=bin\Release\lib\monoandroid11.0
 
-nuget pack amap.xamarin.android.location.nuspec -Version 5.6.2
+nuget pack amap.xamarin.android.location.nuspec -Version 6.0.0
 pause
